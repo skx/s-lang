d50255750b898054cef076c66296c11c7be24876
Steve Kemp <steve@steve.org.uk>
2026-06-09 05:38:07 +0300

    Fixed two new errors found by the fuzzer.
    One nasty one with functions calling themselves with self-referential
    arguments, and one obvious one with the switch-statements.
    
    This closes #162.

--- fuzz_test.go
+++ fuzz_test.go
@@ -61,8 +61,10 @@ func FuzzProject(f *testing.F) {
 		"'{' after case",                            //
 		"expected case|default, got",                //
 		"only integer literals for CASE statements", //
-		"pragma key must be an ident",               // pragma
-		"pragma value must be an ident",             // pragma
+		"switch-statement should only have one default block",
+		"pragma key must be an ident",                // pragma
+		"pragma value must be an ident",              // pragma
+		"invalid self-referential function argument", // function call argument
 	}
 
 	//
