fdca3a34f56110aa9c21273c37930080c6e481d6
Steve Kemp <steve@steve.org.uk>
2026-06-09 18:00:40 +0300

    Add PrefixExpr, and reordered all entries alphabetically.

--- parser/ast.go
+++ parser/ast.go
@@ -1,3 +1,5 @@
+// ast.go - holds our AST definitions.
+
 package parser
 
 import (
@@ -5,159 +7,50 @@ import (
 	"github.com/skx/s-lang/lexer"
 )
 
-// Statement represents a single statement.
-type Statement interface {
-}
-
-// Program represents a complete program.
-//
-// Programs contain Statements.
+// Program represents a complete program, which consists of an arbitrary number of statements.
 type Program struct {
 	// Statements is the set of statements which the program is comprised of.
 	Statements []Statement
 }
 
+// Statement represents a single statement.
+type Statement interface {
+}
+
 // Expr is the interface for expression (maths) parsing.
 type Expr interface {
+	// expr is used to enforce types on our expressions.
 	expr()
 
 	// String returns the value of a given expression.
 	String() string
 }
 
-// FunctionCallExpr holds a reference to a function to call.
-type FunctionCallExpr struct {
-	// Name is the name of the function we should call.
-	Name string
-
-	// Arguments are what we'll setup for the function
-	Arguments []Expr
-}
-
-// IndexExpr holds an indexing operation: expr[index]
-type IndexExpr struct {
-	// Left is a reference to the thing we're indexing
-	Left Expr
-
-	// Index is the index we're going to retrieve.
-	Index Expr
-}
-
-// String converts this structure to a string.
-func (i *IndexExpr) String() string {
-	return fmt.Sprintf("%s[%s]",
-		i.Left.String(),
-		i.Index.String())
-}
-
-func (IndexExpr) expr() {}
-
-// IndexAssign holds an indexing operation: expr[index] = val
-type IndexAssign struct {
-	// Name is the name of the thing we're indexing.
-	Left Expr
-
-	// Index is the index we're going to update.
-	Index Expr
-
-	// Expression is evaluated to set the value.
-	Expression Expr
-}
-
-// String converts this structure to a string.
-func (i *IndexAssign) String() string {
-	return fmt.Sprintf("%s[%s] = %s",
-		i.Left.String(),
-		i.Index.String(),
-		i.Expression.String())
-}
-
-func (IndexAssign) expr() {}
-
-// String returns the value of the given expression.
-func (f *FunctionCallExpr) String() string {
-	return fmt.Sprintf("%s(%s)", f.Name, fmt.Sprintf("%v", f.Arguments))
-}
-func (FunctionCallExpr) expr() {}
-
-// IntegerLiteral holds a literal integer, positive or negative.
-type IntegerLiteral struct {
-	Value int64
-}
-
-// String returns the value of the given expression.
-func (n *IntegerLiteral) String() string {
-	return fmt.Sprintf("%d", n.Value)
-}
-func (IntegerLiteral) expr() {}
-
-// FloatLiteral holds a literal float, positive or negative.
-type FloatLiteral struct {
-	Value float64
-}
-
-// String returns the value of the given expression.
-func (n *FloatLiteral) String() string {
-	return fmt.Sprintf("%f", n.Value)
-}
-func (FloatLiteral) expr() {}
-
-// StringLiteral holds a literal String.
-type StringLiteral struct {
-	Value string
-}
-
-// String returns the value of the given expression.
-func (s *StringLiteral) String() string {
-	return fmt.Sprintf("\"%s\"", s.Value)
-}
-func (StringLiteral) expr() {}
-
-// VariableExpr holds a variable reference.
-type VariableExpr struct {
-	Name string
-}
-
-// String returns the value of the given expression.
-func (v *VariableExpr) String() string {
-	return (v.Name)
-}
-func (VariableExpr) expr() {}
-
-// BinaryExpr holds a binop ("a + b", or similar).
-type BinaryExpr struct {
-	Left  Expr
-	Op    lexer.TokenType
-	Right Expr
-}
-
-// String returns the value of the given expression.
-func (b *BinaryExpr) String() string {
-	return fmt.Sprintf("%s %s %s",
-		b.Left.String(),
-		b.Op,
-		b.Right.String())
-}
-
-func (BinaryExpr) expr() {}
-
-type PostfixExpr struct {
-	Expr Expr
-	Op   lexer.TokenType
-}
-
-// String returns the value of the given expression.
-func (p *PostfixExpr) String() string {
-	return fmt.Sprintf("%s %s",
-		p.Expr.String(),
-		p.Op)
-}
-
-func (PostfixExpr) expr() {}
+////
+//// Now the more concrete statements
+////
+//// Ordered alphabetically.
+////
 
 // Break holds a break-statement, these are only valid within a while loop.
 type Break struct {
 	// Empty
+}
+
+// Case handles a specific case within a switch statement.
+//
+// We support a default-handler if no other case matches, but it is an error to
+// have more than one default case.
+type Case struct {
+
+	// Default branch?
+	Default bool
+
+	// Expression is the thing we match
+	Expression Expr
+
+	// Statements holds the code to execute if there is a match.
+	Statements []Statement
 }
 
 // Continue holds a continue-statement, these are only valid within a while loop.
@@ -173,17 +66,6 @@ type Data struct {
 	Text string
 }
 
-// FunctionParameter is a structure to hold details of the
-// parameters a function accepts.  It is used to allow an
-// optional default to be specified for each parameter.
-type FunctionParameter struct {
-	// Name is the name of the parameter
-	Name string
-
-	// Default, if set, is the value of the parameter
-	Default Expr
-}
-
 // Function holds details about (user-defined) functions.
 type Function struct {
 	// Name is the name of the function that is being defined
@@ -194,6 +76,17 @@ type Function struct {
 
 	// Statements are the body of the function
 	Statements []Statement
+}
+
+// FunctionParameter is a structure to hold details of the
+// parameters a function accepts.  It is used to allow an
+// optional default to be specified for each parameter.
+type FunctionParameter struct {
+	// Name is the name of the parameter
+	Name string
+
+	// Default, if set, is the value of the parameter
+	Default Expr
 }
 
 // If is our conditional operation.
@@ -226,6 +119,19 @@ type Let struct {
 	Expression Expr
 }
 
+// Pragma holds an arbitrary key=value entry.
+//
+// We use this only to modify the "size" of index-operations against strings/memory
+// at the moment.  But it is possible we might do more in the future.
+type Pragma struct {
+
+	// Key contains the key of the pragma entry.
+	Key string
+
+	// Value is the value stored with the specified key.
+	Value string
+}
+
 // Return terminates execution of a function.
 //
 // It is special because there might be an expression, or there might not be.
@@ -236,16 +142,6 @@ type Return struct {
 
 	// Expression is used for the return value, if specified.
 	Expression Expr
-}
-
-// While is our looping operation which allows a block of code to
-// be repeated while a condition evaluates to a true-value.
-type While struct {
-	// Expression is the expression we evaluate each time through the loop.
-	Expression Expr
-
-	// Statements are the things we execute while the condition is true.
-	Statements []Statement
 }
 
 // Switch handles a switch statement
@@ -259,31 +155,177 @@ type Switch struct {
 	Choices []*Case
 }
 
-// Case handles a specific case within a switch statement.
-//
-// We support a default-handler if no other case matches, but it is an error to
-// have more than one default case.
-type Case struct {
-
-	// Default branch?
-	Default bool
-
-	// Expression is the thing we match
+// While is our looping operation which allows a block of code to
+// be repeated while a condition evaluates to a true-value.
+type While struct {
+	// Expression is the expression we evaluate each time through the loop.
 	Expression Expr
 
-	// Statements holds the code to execute if there is a match.
+	// Statements are the things we execute while the condition is true.
 	Statements []Statement
 }
 
-// Pragma holds an arbitrary key=value entry.
+////
+//// Now the more concrete expression-types.
+////
+//// Ordered alphabetically.
+////
+
+// BinaryExpr holds a binary expression, i.e. two arguments with
+// an operation applied to them.
+type BinaryExpr struct {
+	// Left represents the left-side of the expression.
+	Left Expr
+
+	// Right holds the right-side of the expression.
+	Right Expr
+
+	// Op holds the operation to be carried out upon the two
+	// operands (+, -, etc)
+	Op lexer.TokenType
+}
+
+// String returns the value of the given expression.
+func (b *BinaryExpr) String() string {
+	return fmt.Sprintf("%s %s %s",
+		b.Left.String(),
+		b.Op,
+		b.Right.String())
+}
+
+func (BinaryExpr) expr() {}
+
+// FloatLiteral holds a literal float, positive or negative.
+type FloatLiteral struct {
+	Value float64
+}
+
+// String returns the value of the given expression.
+func (n *FloatLiteral) String() string {
+	return fmt.Sprintf("%f", n.Value)
+}
+func (FloatLiteral) expr() {}
+
+// FunctionCallExpr represents a function-call..
+type FunctionCallExpr struct {
+	// Name is the name of the function we should call.
+	Name string
+
+	// Arguments are what we'll setup for the function
+	Arguments []Expr
+}
+
+// String returns the value of the given expression.
+func (f *FunctionCallExpr) String() string {
+	return fmt.Sprintf("%s(%s)", f.Name, fmt.Sprintf("%v", f.Arguments))
+}
+func (FunctionCallExpr) expr() {}
+
+// IndexAssign holds an indexing operation: expr[index] = val
+type IndexAssign struct {
+	// Name is the thing we're indexing.
+	Left Expr
+
+	// Index is the index we're going to update.
+	Index Expr
+
+	// Expression is evaluated to set the value.
+	Expression Expr
+}
+
+// String converts this structure to a string.
+func (i *IndexAssign) String() string {
+	return fmt.Sprintf("%s[%s] = %s",
+		i.Left.String(),
+		i.Index.String(),
+		i.Expression.String())
+}
+
+func (IndexAssign) expr() {}
+
+// IndexExpr holds an indexing operation: expr[index]
+type IndexExpr struct {
+	// Left is the thing we're indexing
+	Left Expr
+
+	// Index is the index we're going to retrieve.
+	Index Expr
+}
+
+// String converts this structure to a string.
+func (i *IndexExpr) String() string {
+	return fmt.Sprintf("%s[%s]",
+		i.Left.String(),
+		i.Index.String())
+}
+
+func (IndexExpr) expr() {}
+
+// IntegerLiteral holds a literal integer, positive or negative.
+type IntegerLiteral struct {
+	Value int64
+}
+
+// String returns the value of the given expression.
+func (n *IntegerLiteral) String() string {
+	return fmt.Sprintf("%d", n.Value)
+}
+func (IntegerLiteral) expr() {}
+
+// PrefixExpr holds prefix expressions.
 //
-// We use this only to modify the "size" of index-operations against strings/memory
-// at the moment.  But it is possible we might do more in the future.
-type Pragma struct {
+// We already cope with negative numbers, so we don't have a unary-minus,
+// our only prefix expression is ! for the logical not.
+type PrefixExpr struct {
+	Expr Expr
+	Op   lexer.TokenType
+}
 
-	// Key contains the key of the pragma entry.
-	Key string
+// String returns the value of the given expression.
+func (p *PrefixExpr) String() string {
+	return fmt.Sprintf("%s%s",
+		p.Op,
+		p.Expr.String())
+}
 
-	// Value is the value stored with the specified key.
+func (PrefixExpr) expr() {}
+
+// PostfixExpr holds a postfix expression,
+type PostfixExpr struct {
+	// Expr holds the value we're applying the operation to.
+	Expr Expr
+
+	// Op holds the operation we're applying (++, --, etc).
+	Op lexer.TokenType
+}
+
+// String returns the value of the given expression.
+func (p *PostfixExpr) String() string {
+	return fmt.Sprintf("%s %s",
+		p.Expr.String(),
+		p.Op)
+}
+
+func (PostfixExpr) expr() {}
+
+// StringLiteral holds a literal String.
+type StringLiteral struct {
 	Value string
 }
+
+// String returns the value of the given expression.
+func (s *StringLiteral) String() string {
+	return fmt.Sprintf("\"%s\"", s.Value)
+}
+func (StringLiteral) expr() {}
+
+// VariableExpr holds a variable reference.
+type VariableExpr struct {
+	Name string
+}
+
+// String returns the value of the given expression.
+func (v *VariableExpr) String() string {
+	return (v.Name)
+}
+func (VariableExpr) expr() {}
